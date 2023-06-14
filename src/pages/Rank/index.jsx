f626f5757f8df4ac9fd9c3813ec538025389fb87
blarth <jminocente@gmail.com>
2023-06-14 03:29:22 -0300

    feat: img deploy

--- src/pages/Rank/index.jsx
+++ src/pages/Rank/index.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import useRank from "../../hooks/useRank";
-import { useEffect } from "react";
 
 export default function Rank() {
   const {rank} = useRank();
